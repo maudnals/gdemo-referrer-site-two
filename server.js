f8d42ba20d75f01e2602b285d872205d0347766a
Glitch (site-two) <none>
2020-06-04 07:41:56 +0000

    🏃🐙 Checkpoint ./server.js:14983989/279 ./.prettierrc.json:14983989/102

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const app = express()
 
-app.use(cors());
+app.use(cors())
 // use a view engine for convenience
 // so I can display server-collected info (= the referrer in our case) in the HTML markup
 // http://expressjs.com/en/guide/using-template-engines.html
 app.set('view engine', 'pug')
 
-app.use(express.static("public"));
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-  const referer = req.get('Referer');
-  res.render('index', { message: `${referer}`});
+  const referer = req.get('Referer')
+  res.render('index', { message: `${referer}` })
 })
 
 app.get('/ref', function (req, res) {
-  const referer = req.get('Referer');
-  console.log("referer", referer)
-  res.json(referer);
+  const referer = req.get('Referer')
+
+  const origin = req.get('Origin')
+
+  console.log('referer', referer)
+  console.log('origin', origin)
+  res.json(referer)
 })
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+  console.log('Your app is listening on port ' + listener.address().port)
+})
